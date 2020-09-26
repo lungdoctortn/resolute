e5b213e4264fd846e03bd5a7a67a7db80d57b309
Dave <davidjhenson@gmail.com>
2020-09-25 22:08:57 -0500

    Fixes made on entire site

--- template-parts/content-archive.php
+++ template-parts/content-archive.php
@@ -2,6 +2,8 @@
 
 <article <?php post_class(array( 'class' => 'blog-post' ) ); ?>>
 	<a href="<?php the_permalink(); ?>"><h2><?php the_title(); ?></h2></a>
+
+	
 	<div class="thumbnail">
 		<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'blog-image', array( 'class' => 'archive-image' ) ); ?></a>
 	</div>
@@ -10,8 +12,10 @@
 		<p>Categories: <?php the_category( ', ' ); ?></p>
 		<p><?php the_tags('Tags: ', ', ' ); ?></p>		
 	</div>
+
 		<!-- <p class="archive__paragraph" ><?php the_excerpt(); ?></p> -->
 		<?php add_filter("excerpt_length", "custom_excerpt_length_short", 999); ?>
 			<p class="archive__paragraph"><?php the_excerpt(); ?></p>
 		<?php remove_filter("excerpt_length", "custom_excerpt_length_short", 999); ?>
 </article>
+<div class="clear"></div>
